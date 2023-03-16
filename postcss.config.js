448820e2f8fdaa1362cd83ae744ec128e4f17f48
Yash-Bavishi <bavishiys63@gmail.com>
2023-03-16 12:35:17 +0530

    Frontend

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	plugins: {
+		tailwindcss: {
+		},
+		'postcss-focus-visible': {
+			replaceWith: '[data-focus-visible-added]',
+		},
+		autoprefixer: {},
+	},
+}
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+export function Footer() {
+	return (
+		<div className="footer">
+			<div className="contact">
+				<p>SAD</p>
+				<p>SAD</p>
+				<p>SAD</p>
+			</div>
+			<div className="address">
+				<p>BAD</p>
+				<p>BAD</p>
+				<p>BAD</p>
+			</div>
+			<div className="email">Contact 98</div>
+
+		</div>
+	)
+}
